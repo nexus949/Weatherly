858db47532541faf4cf845737c99868ab1d069a6
Dipanjan Mitra <90700314+itsdstar@users.noreply.github.com>
2024-06-05 17:46:27 +0000

    TYPO

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ async function getResponse() {
     try {
         place = document.querySelector(".searchBox").value;
 
-        response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3ec38af15e3c4d18a2b144806242204&q=${place}&aqi=yes`);
+        response = await fetch(`https://api.weatherapi.com/v1/current.json?key=API_KEY&q=${place}&aqi=yes`);
         if (!response.ok) {
             console.error(`Error: ${response.status} - ${response.statusText}`);
             return;
